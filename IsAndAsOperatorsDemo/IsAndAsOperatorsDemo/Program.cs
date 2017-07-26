624f97e870ff3d27c58454c8e9d8d44cdf9f1730
Gashaw1 <akalc2004@yahoo.com>
2017-07-25 20:16:12 -0400

    .

---
+++ IsAndAsOperatorsDemo/IsAndAsOperatorsDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAndAsOperatorsDemo
+{
+    class Parent
+    {
+        int parentID { get; set; }
+        string parentMiddleName { get; set; }
+    }
+    class Child : Parent
+    {
+        int childId { get; set; }
+        string childLastName { get; set; }
+    }
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Parent parent = new Parent();
+            Child child = new Child();
+            //return true because Child class inherited Parent class
+            bool IsChild = child is Parent;
+            //this time return false because the base class can't be an object to the chld class
+            bool IsPrent = parent is Child;
+
+            castToObject(parent);
+            castToObject(child);
+
+        }
+        public static bool castToObject(object obj)
+        {            
+            Child parent = obj as Child;
+
+            if (parent != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }           
+        }
+    }
+}
