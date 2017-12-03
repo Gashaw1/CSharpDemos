2406887b9e37c2624e1ed53d76a134e9dea09d02
Gashaw1 <akalc2004@yahoo.com>
2017-12-02 20:11:45 -0500

    .

---
+++ CalculateHashCode/CalculateHashCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateHashCode
+{
+    public class CalcHashCode
+    {
+        byte[] hashA;
+        byte[] hashB;
+        string data = "sample texts one";
+        string data2 = "sample texts one";
+
+        void CreateHash()
+        {
+            UnicodeEncoding byteConverter = new UnicodeEncoding();
+            SHA256 sha256 = SHA256.Create();
+
+            hashA = sha256.ComputeHash(byteConverter.GetBytes(data));
+            hashB = sha256.ComputeHash(byteConverter.GetBytes(data2));
+        }
+        public bool Equal()
+        {
+            CreateHash();
+            return hashA.SequenceEqual(hashB); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcHashCode calHash = new CalcHashCode();
+            var result = calHash.Equal();
+            Console.WriteLine(result);
+            Console.Read();
+
+        }
+    }
+}
---
+++ CalculateHashCode/CalculateHashCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CalculateHashCode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculateHashCode")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f891a57-a905-4868-8505-6215a6aa1517")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
