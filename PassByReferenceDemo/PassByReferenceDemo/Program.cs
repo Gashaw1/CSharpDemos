157abd1dddcadced947f542c3c575966848805c7
Gashaw1 <akalc2004@yahoo.com>
2017-12-04 11:06:51 -0500

    .

---
+++ PassByReferenceDemo/PassByReferenceDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByReferenceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bValidInteger = false;
+            int value = 0;
+            do
+            {
+                Console.WriteLine("Enter an integer:");
+                bValidInteger = GetValidInteger(ref value);
+            }
+            while (!bValidInteger);
+            {
+                Console.WriteLine("You entered a valide integer", +value);
+
+            }
+            Console.Read();
+
+        }
+
+        static bool GetValidInteger(ref int val)
+        {
+            string strLine = Console.ReadLine();
+            int number;
+
+            if (!int.TryParse(strLine, out number))
+            {
+                return false;
+            }
+            else
+            {
+                val = number;
+                return true;
+            }
+        }
+    }
+}
