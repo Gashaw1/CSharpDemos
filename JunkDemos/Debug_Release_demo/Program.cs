82fc9e45550c65d987852b86862c2112fe27ac22
Gashaw1 <akalc2004@yahoo.com>
2017-12-09 17:00:17 -0500

    .

---
+++ JunkDemos/Debug_Release_demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Debug_Release_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class CalcInterest
+    {
+
+        private static decimal cInterest(decimal loanAmount, int lonTerm, decimal loanRate)
+        {
+            decimal result = loanAmount * loanRate * loanRate;
+#if DEBUG
+            LogLine("Interest Amount : ", result.ToString("C"));
+#endif
+            return result;
+        }
+        [Conditional("DEBUG")]
+        public static void LogLine(string message, string detail)
+        {
+            Console.WriteLine("Log: {0} = {1}", message, detail);
+        }
+    }
+}
